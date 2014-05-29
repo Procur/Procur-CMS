4198fbbb22042fd3e1b3056a81aae5b5af7f3874
Will Ricketts <wricketts@procur.com>
2014-05-29 09:54:18 -0400

    Added models and controllers for blogs. Added index action to MarketingPost controller

---
+++ api/models/MarketingPost.js
@@ -0,0 +1,33 @@
+/**
+ * MarketingPost
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+  	title: {
+      type: 'string',
+      required: true
+    },
+    content: {
+      type: 'text',
+      required: 'true'
+    },
+    images: {
+      type: 'json',
+    },
+    published: {
+      type: 'boolean',
+      required: 'true'
+    },
+    thumbnail: {
+      type: 'json'
+    }
+  }
+
+};
