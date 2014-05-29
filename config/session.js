dea02bfa5912ef4b3a035a10325f26f753d06729
Will Ricketts <wricketts@procur.com>
2014-05-29 09:30:25 -0400

    Added Procur-core session secret

--- config/session.js
+++ config/session.js
@@ -1,7 +1,7 @@
 /**
  * Session
- * 
- * Sails session integration leans heavily on the great work already done by Express, but also unifies 
+ *
+ * Sails session integration leans heavily on the great work already done by Express, but also unifies
  * Socket.io with the Connect session store. It uses Connect's cookie parser to normalize configuration
  * differences between Express and Socket.io and hooks into Sails' middleware interpreter to allow you
  * to access and auto-save to `req.session` with Socket.io the same way you would with Express.
@@ -14,8 +14,8 @@ module.exports.session = {
 
   // Session secret is automatically generated when your new app is created
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
-  // forcing them to log in again. 
-  secret: '7987165803030090ea06827687b2d726'
+  // forcing them to log in again.
+  secret: '8666c1f82163ab798e9792f1911cadcb'
 
 
   // In production, uncomment the following lines to set up a shared redis session store
