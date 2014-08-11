0ecd0267dc189a0beb9f155f1d74595e8d3d0e6d
Trey Schneider <tschneider@procur.com>
2014-08-11 16:14:03 -0400

    disable PR

--- assets/js/pressreleaseComingSoon.js
+++ assets/js/pressreleaseComingSoon.js
@@ -1,3 +1,13 @@
-$('#companyBlogIndex').hover( function() {
-  $(this).find('a').text('New Text');
-});
+
+$('.hoverPR').removeAttr("href");
+
+
+$('.hoverPR').hover(
+	function(){
+		prevHtml = $(this).html();
+		$(this).html('Coming Soon');
+	},
+	function(){
+		$(this).html(prevHtml);
+	}
+	);
